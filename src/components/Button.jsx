c2684ee39e7964f4395f86db366cd9515debaa79
shazab-saifi <shazebsaifi92@gmail.com>
2024-10-10 19:35:53 +0530

    Routing done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Button = ({ children, className }) => {
     return (
-        <Link to='/signup' className={`bg-black text-white transition-colors hover:bg-#a34cdb inline-flex justify-center items-center text-center rounded-[10rem] font-medium pt-3 pb-[0.55rem] px-6 cursor-pointer ${className || ''}`}>
+        <Link to='/signup' className={`bg-black text-white transition-colors hover:bg-#a34cdb inline-flex justify-center items-center text-center rounded-[10rem] font-medium py-3 px-6 cursor-pointer ${className || ''}`}>
             {children}
         </Link>
     )
